81dd19d43629955406b2f4b0a105449bfa03da46
Rafael Soares <rafael.dvlpr@gmail.com>
2022-04-30 21:36:50 -0300

    test client send request creates client

--- client/client_test.go
+++ client/client_test.go
@@ -12,6 +12,7 @@ import (
 
 var mockServer *httptest.Server
 var testClient *rapidpro.Client
+var token = "token123"
 
 func NewClient(token string) *rapidpro.Client {
 	c := &rapidpro.Client{
@@ -42,4 +43,13 @@ func TestClient_SendRequestError(t *testing.T) {
 	rapidproErr := err.(*rapidpro.RapidproRestError)
 	assert.Nil(t, resp)
 	assert.Equal(t, 400, rapidproErr.Status)
+}
+
+func TestClient_SendRequestCreatesClient(t *testing.T) {
+	c := &rapidpro.Client{
+		Credentials: &rapidpro.Credentials{Token: token},
+	}
+	resp, err := c.SendRequest(http.MethodGet, mockServer.URL, nil, nil, nil)
+	assert.NoError(t, err)
+	assert.Equal(t, 200, resp.StatusCode)
 }
